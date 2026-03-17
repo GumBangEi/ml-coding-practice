0da9637c92020f9c51d92a011c45f638324b6b9f
임창순 <tommy11109169@gmail.com>
2026-03-17 11:02:43 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -38,5 +38,5 @@ print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
 print(sample_df.iloc[0:3, 2:4])           # 칼럼까지 동시에 인덱싱
 
-print(sample_df.drop)
+print(sample_df.drop[['var_1', 'var_3'], axis=1])
 
