6fca4a729bdbb5601db543aa8fa7d7d4e69a52cc
임창순 <tommy11109169@gmail.com>
2026-03-23 21:02:34 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -200,3 +200,8 @@ plt.boxplot([titanic[titanic['Pclass'] == 1]['Age'],
              labels=['1st Class', '2nd Class', '3rd Class'])
 
 plt.title('Box Plot for Age by Pclass')
+plt.xlabel('Pclass')
+plt.ylabel('Age')
+plt.savefig('Figure09.png')
+plt.close()
+
