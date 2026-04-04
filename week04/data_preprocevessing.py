7b71eeffe1b866d73b3bd9e01b7fec25aa9e2917
임창순 <tommy11109169@gmail.com>
2026-04-04 19:52:26 +0900

    Updated Changes

--- week04/data_preprocevessing.py
+++ week04/data_preprocevessing.py
@@ -58,5 +58,14 @@ housing_tr = pd.DataFrame(X, columns=housing_num.columns,
 housing_tr.loc[null_rows_idx].head()
 
 # 이상치 삭제
+from sklearn.ensemble import IsolationForest
+
+isolation_forest = IsolationForest(random_state=42)
+outlier_pred = isolation_forest.fit_predict(X)
+
+outlier_pred
+
+housing = housing.iloc[outlier_pred == 1]
+housing_labels = housing_labels.iloc[outlier_pred == 1]
 
 # 텍스트와 범주형 특성 다루기
