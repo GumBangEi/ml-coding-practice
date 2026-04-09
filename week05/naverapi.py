d748c2954629267fdf975a99261dd68484395559
임창순 <tommy11109169@gmail.com>
2026-04-09 10:01:54 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -7,6 +7,7 @@ client_id = 'Client ID'
 client_secret = 'Client Secret'
 
 def main():
+
     node = 'news'                                               # 크롤링할 대상
     srcText = input('검색어를 입력하세요: ')
 
@@ -35,6 +36,7 @@ def main():
     print("가져온 데이터 : %d 건" %(cnt))
     print('%s_naver_%s.json SAVED' % (srcText, node))
 
+
 def getNaverSearch(node, srcText, page_start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
@@ -47,6 +49,7 @@ def getNaverSearch(node, srcText, page_start, display):
         return None
     else:
         return json.loads(responseDecode)
+    
     
 def getRequestUrl(url):
     req = urllib.request.Request(url)
