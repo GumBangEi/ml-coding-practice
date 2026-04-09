b971b853315e96e5e7df9f86edb045903d52b1bc
임창순 <tommy11109169@gmail.com>
2026-04-09 10:28:24 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -5,3 +5,12 @@ import json
 import pandas as pd
 
 ServiceKey = "3f1b77ae1849eb266bb8b2f1d26896afac4fe8b9f83878a04d47c240d6c9fc00"
+
+"""### [CODE 0]"""
+
+def main():
+    jsonResult = []
+    result = []
+
+    print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
+    
