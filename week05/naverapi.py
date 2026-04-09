784d687d5396451b0cf5ef13f36ca450eebec016
임창순 <tommy11109169@gmail.com>
2026-04-09 09:53:24 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -32,4 +32,10 @@ def main():
                         
         outfile.write(jsonFile)
 
-        
+    print("가져온 데이터 : %d 건" %(cnt))
+    print('%s_naver_%s.json SAVED' % (srcText, node))
+
+def getNaverSearch(node, srcText, page_start, display):
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s"
