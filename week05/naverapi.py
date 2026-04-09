e470a88f64b26380668c44fc7352cf750b3304ef
임창순 <tommy11109169@gmail.com>
2026-04-09 09:41:54 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -7,6 +7,14 @@ client_id = 'Client ID'
 client_secret = 'Client Secret'
 
 def main():
-    node = 'news'                           # 크롤링할 대상
+    node = 'news'                                           # 크롤링할 대상
     srcText = input('검색어를 입력하세요: ')
-    
+
+    cnt = 0
+    jsonResult = []
+
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)    # [CODE 2]
+    total = jsonResponse['total']
+
+    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
+        
