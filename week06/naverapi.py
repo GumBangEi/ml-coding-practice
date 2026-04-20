af8bae8a4875045c3db0780b9a5591810482b408
임창순 <tommy11109169@gmail.com>
2026-04-20 12:57:19 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -7,32 +7,32 @@ client_id = 'yGm5lVQoAkLQ9QLEOeTf'
 client_secret = '4uEbJ4s_fU'
 
 def main():
-    
-    node = 'news'                                           # 크롤링할 대상
+
+    node = 'news'                                             # 크롤링할 대상
     srcText = input('검색어를 입력하세요: ')
 
     cnt = 0
     jsonResult = []
 
-    jsonResponse = getNaverSearch(node, srcText, 1, 100)    # [CODE 2]
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
     total = jsonResponse['total']
 
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
-            getPostData(post, jsonResult, cnt)              # [CODE 3]
+            getPostData(post, jsonResult, cnt)                # [CODE 3]
 
         start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
-    
+
     print('전체 검색 : %d 건' %total)
 
-    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding= 'utf8') as outfile:
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding = 'utf8') as outfile:
         jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
                               ensure_ascii = False)
 
         outfile.write(jsonFile)
-    
+
     print("가져온 데이터 : %d 건" %(cnt))
     print('%s_naver_%s.json SAVED' % (srcText, node))
 
@@ -43,13 +43,13 @@ def getNaverSearch(node, srcText, page_start, display):
     parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
 
     url = base + node + parameters
-    responseDecode = getRequestUrl(url)             #[CODE 1]
+    responseDecode = getRequestUrl(url)               #[CODE 1]
 
     if (responseDecode == None):
         return None
     else:
         return json.loads(responseDecode)
-    
+
 
 def getRequestUrl(url):
     req = urllib.request.Request(url)
@@ -68,16 +68,16 @@ def getRequestUrl(url):
         return None
 
 
-def getPostData(post, jsonResult, cnt):     #[CODE 3]
+def getPostData(post, jsonResult, cnt):  #[CODE 3]
     title = post['title']
     description = post['description']
-    org_link = post['orignallink']
+    org_link = post['originallink']
     link = post['link']
 
-    # %a: 짧은 형식의 요일 이름 (예, 'Mon', 'Tue', 'Wed', ... )
-    # %d: 일 (예, 01, 02, 03, ... )
-    # %b: 짧은 형식의 월 이름 (예, 'Jan', 'Feb', 'Mar', ... )
-    pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S + 0900')
+    # %a: 짧은 형식의 요일 이름 (예, 'Mon', "Tue', "Wed', ... )
+    # %d: 일 (예, 01, 02, 03, ...)
+    # %b: 짧은 형식의 월 이름  (예, 'Jan', 'Feb', 'Mar', ...)
+    pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
     pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
 
     jsonResult.append({
