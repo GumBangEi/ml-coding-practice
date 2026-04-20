0a5f821872040daceeb6dc76b4f435aff0bf70c1
임창순 <tommy11109169@gmail.com>
2026-04-20 13:09:50 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -30,4 +30,11 @@ def main():
 def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
     jsonResult = []
     result = []
-    
+
+    for year in range(nStartYear, nEndYear+1):
+        for month in range(1, 13):
+            yyyymm = "{0}{1:0>2}".format(str(year), str(month))
+            jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)       #[CODE 2]
+            if (jsonData['response']['header']['resultMsg'] == 'OK'):
+                #데이터가 없는 마지막 항목인 경우 -------------------------------
+                print("데이터 없음.... \n제공되는 통계 데이터는 %s년 %s월까지 입니다.")
