1d431c910c387ee9deaefdf49ce8bbbedd8c7dae
임창순 <tommy11109169@gmail.com>
2026-04-20 13:00:50 +0900

    Updated Changes

---
+++ week06/tourism.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import urllib.request
+import datetime
+import json
+import pandas as pd
+
+ServiceKey = "3f1b77ae1849eb266bb8b2f1d26896afac4fe8b9f83878a04d47c240d6c9fc00"
+
+"""### [CODE 0]"""
+
+def main():
+    jsonResult = []
+    result = []
+
+    print("<< 국내 입주한 외국인의 통계 데이터를 수잡합니다. >>")
+    nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) : ')
+    nStartYear = int(input('데이터를 몇 년부터 수집할까요? : '))
+    nEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
+    ed_cd = "E"                                
