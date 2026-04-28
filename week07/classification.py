b5527d22f26c81e6c588f4a29e7b5e83b2437f24
임창순 <tommy11109169@gmail.com>
2026-04-28 13:04:40 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -70,5 +70,4 @@ cm = confusion_matrix(y_train_5, y_train_pred)
 print(cm)
 
 y_train_perfect_predicitions = y_train_5    # 완벽한 분류기일 경우
-
-print()
+print(confusion_matrix(y_train_5, y_train_perfect_predicitions))
