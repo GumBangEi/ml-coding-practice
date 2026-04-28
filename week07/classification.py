6b2614c75a4ef842d518b40158834499f53808ad
임창순 <tommy11109169@gmail.com>
2026-04-28 16:37:43 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -73,7 +73,8 @@ y_train_perfect_predicitions = y_train_5    # 완벽한 분류기일 경우
 print(confusion_matrix(y_train_5, y_train_perfect_predicitions))
 
 # 정밀도와 재현율
-from sklearn.matrics import precision_score, recall_score
+from sklearn.metrics import precision_score, recall_score
 
 print(precision_score(y_train_5, y_train_pred))     # == 3530 / (687 + 3530)
 
+print(recall_score(y_train_5, y_train_pred))        # 3530 / 
